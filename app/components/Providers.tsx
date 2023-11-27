28c89e306f5b67a79c0ae47c2baa30e09cb1b659
amer1 <amer1@sub.uni-goettingen.de>
2023-11-27 16:09:29 +0100

    feat: use shopping cart provider

---
+++ app/components/Providers.tsx
@@ -0,0 +1,22 @@
+"use client"; //it is a user client component
+
+import { ReactNode } from "react";
+import { CartProvider as USCProvider } from "use-shopping-cart";
+
+export default function CartProvider({ children }: { children: ReactNode }) {
+  return (
+    <USCProvider
+      mode="payment"
+      cartMode="client-only"
+      stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
+      successUrl="http://localhost:3000/stripe/success"
+      cancelUrl="http://localhost:3000/stripe/error"
+      currency="USD"
+      billingAddressCollection={false}
+      shouldPersist={true} //if the user reload the page all the cart items will be stored in the local storage
+      language="en-US"
+    >
+      {children}
+    </USCProvider>
+  );
+}
