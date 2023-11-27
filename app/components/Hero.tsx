7299e1aac427046bd221e5cb0abdf028d2c92b62
amer1 <amer1@sub.uni-goettingen.de>
2023-11-27 16:07:29 +0100

    feat: add hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,16 +2,32 @@ import Image from "next/image";
 import { client, urlFor } from "../lib/sanity";
 import Link from "next/link";
 
-async function getData() {
-  const query = "*[_type == 'heroImage'][0]";
+async function getHero() {
+  const imagequery = "*[_type == 'heroImage'][0]";
 
-  const data = await client.fetch(query);
+  const herodata = await client.fetch(imagequery);
 
-  return data;
+  return herodata;
+}
+
+async function getCategories() {
+  const categoriesquery = `*[_type == 'category']{
+    _id,
+    name,
+    "slug":slug.current
+  }`;
+
+  const categoriesdata = await client.fetch(categoriesquery);
+
+  return categoriesdata;
 }
 
 export default async function Hero() {
-  const data = await getData();
+  const herodata = await getHero();
+  const categoriesdata = await getCategories();
+
+  //console.log(categoriesdata);
+
   return (
     <section className="mx-auto max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8">
       <div className="mb-8 flex flex-wrap justify-between md:mb-16">
@@ -28,7 +44,7 @@ export default async function Hero() {
         <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
           <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
             <Image
-              src={urlFor(data.image1).url()}
+              src={urlFor(herodata.image1).url()}
               alt="Great Photo"
               className="h-full w-full object-cover object-center"
               priority
@@ -39,7 +55,7 @@ export default async function Hero() {
 
           <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
             <Image
-              src={urlFor(data.image2).url()}
+              src={urlFor(herodata.image2).url()}
               alt="Great Photo"
               className="h-full w-full object-cover object-center"
               width={500}
@@ -50,27 +66,16 @@ export default async function Hero() {
         </div>
       </div>
 
-      <div className="flex flex-col items-center justify-between gap-8 md:flex-row">
-        <div className="flex h-12 w-64 divide-x overflow-hidden rounded-lg border">
+      <div className="flex h-12 w-64 divide-x overflow-hidden rounded-lg border">
+        {categoriesdata.map((category: any) => (
           <Link
-            href="/Men"
-            className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
+            key={category._id}
+            href={`/category/${category.name}`}
+            className="flex w-2/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
           >
-            Men
+            {category.name}
           </Link>
-          <Link
-            href="/Women"
-            className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
-          >
-            Women
-          </Link>
-          <Link
-            href="/Teens"
-            className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
-          >
-            Teens
-          </Link>
-        </div>
+        ))}
       </div>
     </section>
   );
